fede114d1f523888eaa689c1ac1d12af7ffd9c04
meldy8 <idapeam@gmail.com>
2024-05-04 14:50:43 +0000

    Ajout de la vue et du template de détail blog post ainsi que du CSS pour le site.

--- blog/views.py
+++ blog/views.py
@@ -9,9 +9,11 @@ def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'blog/post_list.html', {'posts': posts})
     Post.objects.get(pk=pk)
+
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
     return render(request, 'blog/post_detail.html', {'post': post})
+
 def post_new(request):
     if request.method == "POST":
         form = PostForm(request.POST)
